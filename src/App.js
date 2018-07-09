d2f505788f13a7cddd189e0f6b5c06a7d7ea9a1a
mchen3 <tap135@hotmail.com>
2018-07-08 21:55:38 -0400

    Forty by HTML5 Up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+// import './App.css';
+
+// import './assets/css/main.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+
+
+<div className="container">
+    <div className="row">
+      <div className="col-sm">
+        One of threefsdfsd columns
       </div>
+      <div className="col-sm">
+        One of three columns
+      </div>
+      <div className="testDiv">
+        One of three columns
+      </div>
+
+      <h1> <div className="col-sm">Testing </div> </h1>
+      <h2> 2nd element </h2>
+
+    </div>
+    </div>
+
+
+
+
+      </div>
+
+
+
+
+
     );
   }
 }
